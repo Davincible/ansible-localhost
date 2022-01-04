de224b46ee7c0dd1bf00f47f74c4b15ca9ef5f5f
Davincible <david.brouwer.99@gmail.com>
2022-01-04 17:49:04 +0200

    update install commands

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 sudo pacman -Syu
 sudo pacman -S --needed ansible ansible-core ansible-lint
 sudo pacman -S --needed yay base-devel
-yay -S --norebuild --nocleanmenu --nodiffmenu ansible-aur
+yay -S --noprovides --norebuild --nocleanmenu --nodiffmenu ansible-aur
 ansible-galaxy collection install community.general
