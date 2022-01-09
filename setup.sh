ac06a8a9522ec8828926371cc83c06fb7b534ede
Davincible <david.brouwer.99@gmail.com>
2022-01-09 20:33:02 +0200

    better

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo pacman -S pacman-mirrors
+sudo rankmirrors -f 6
 sudo pacman -Syu
 sudo pacman -S --needed ansible ansible-core ansible-lint
 sudo pacman -S --needed yay base-devel
-yay -S --noprovides --norebuild --nocleanmenu --nodiffmenu ansible-aur
+yay -SYgyu --noprovides --norebuild --nocleanmenu --nodiffmenu ansible-aur
 ansible-galaxy collection install community.general
