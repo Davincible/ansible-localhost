37bb27da56707a542f7aaa282aca296113f38cdc
Davincible <david.brouwer.99@gmail.com>
2022-01-11 14:52:21 +0100

    idk updates

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 sudo pacman -S pacman-mirrors
-sudo rankmirrors -f 6
 sudo pacman -Syu
 sudo pacman -S --needed ansible ansible-core ansible-lint
 sudo pacman -S --needed yay base-devel
-yay -SYgyu --noprovides --norebuild --nocleanmenu --nodiffmenu ansible-aur
+sudo rankmirrors -f 6
+sudo pacman -Syu
+yay -S --noprovides --norebuild --nocleanmenu --nodiffmenu ansible-aur
 ansible-galaxy collection install community.general
+ansible-galaxy install robertdebock.cargo
+sudo usermod -aG video $USER
+sudo usermod -aG docker $USER
+sudo usermod -aG input $USER
+sudo usermod -aG audio $USER
+sudo usermod -aG storage $USER
